372e501b6985eb61b168f70c0543717f81186ffa
human <dip@dip-office.net>
2016-10-10 17:36:09 +0200

    testing ok. ToDo: more tests

--- test/intercept.js
+++ test/intercept.js
@@ -1,13 +1,4 @@
 var web3UtilApi = require('../lib/web3-util');
-const TEST_DATA = [
-  {  args : [3,6,7,9], result: false},
-  {  args : [3,6,6,9], result: true },
-  {  args : [3,6,5,9], result: true },
-  {  args : [3,6,0,9], result: true },
-  {  args : [3,6,0,5], result: true },
-  {  args : [3,6,0,2], result: false},
-  {  args : [3,6,0,3], result: true}
-].forEach(function(data, idx) {
   contract('2. intersect tests', function (accounts) {
     var immoRegistry;
     var resolveEvent;
@@ -22,7 +13,16 @@ const TEST_DATA = [
     beforeEach(function () {
       immoRegistry = ImmoRegistry.deployed();
     });
-
+    
+    const TEST_DATA = [
+      {  args : [3,6,7,9], result: false},
+      {  args : [3,6,6,9], result: true },
+      {  args : [3,6,5,9], result: true },
+      {  args : [3,6,0,9], result: true },
+      {  args : [3,6,0,5], result: true },
+      {  args : [3,6,0,2], result: false},
+      {  args : [3,6,0,3], result: true}
+    ].forEach(function(data, idx) {
     it(idx + ' intersects('+data.args+')=='+data.result+' is valid', function (done) {
       const x1 = data.args[0];
       const y1 = data.args[1];
